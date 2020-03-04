7de72b30176b05a7cdf24e6b119fb061a49f69b5
Nguyễn Vũ Nhật Huy <v.huynvn@vinid.net>
2020-03-04 15:49:55 +0700

    Done DataBingDeep

--- README.md
+++ README.md
@@ -25,3 +25,8 @@ To be consistent between JIT and AOT, you must alter both files!
     - Native: Sẽ không nhận từ parent lẫn global
     - Emulated (default): nếu parent and global có thì sẽ nhận của global.
 
++ Life cycle hook: 
+
+  Khi một component được khởi tạo, sau khi hoàn thành hàm constructor, Angular sẽ gọi đến hook **ngOnChanges**. **ngOnChanges** bắt các thay đổi của giá trị khởi tạo được truyền vào component, được lặp đi lặp lại nhiều lần trong suốt vòng đời của component. - Kết thúc **ngOnChanges**, Angular tiếp tục gọi đến **ngOnInit**. Đây là hook được gọi một lần duy nhất, ngay sau khi **ngOnChanges** đầu tiên hoàn tất. - Tiếp đó, Component tiến hành render view, truyền dữ liệu vào view, Angular gọi đến hook **ngAfterContentInit** và khi hoàn tất gọi đến hook **ngAfterContentChecked** sau khi đã kiểm tra dữ liệu lấy từ bên ngoài vào view. - Khi render view của component, Angular gọi đến **ngAfterViewInit**, sau đó gọi hook **ngAfterViewChecked** khi đã kiểm tra xong dữ liệu trên view của component - Cuối cùng, khi ta gọi đến hàm hủy của Class, Angular sẽ gọi và thực hiện các hành động trong hook **ngOnDestroy** trước khi thực hiện hàm hủy và kết thúc vòng đời của component. ## Cách dùng các component lifecycle hooks
+
+  ngDoCheck: Phát hiện và thực hiện thay đổi. Được gọi mỗi khi tìm thấy thay đổi 
--- ch05-dataBinding/src/app/server-element/server-element.component.ts
+++ ch05-dataBinding/src/app/server-element/server-element.component.ts
@@ -1,4 +1,19 @@
-import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
+import { Component, 
+  OnInit, 
+  Input, 
+  ViewEncapsulation, 
+  OnChanges, 
+  SimpleChanges,
+  DoCheck,
+  AfterContentInit, 
+  AfterContentChecked, 
+  AfterViewInit, 
+  AfterViewChecked, 
+  OnDestroy,
+  ViewChild,
+  ElementRef,
+  ContentChild
+} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -6,11 +21,60 @@ import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./server-element.component.css'],
   encapsulation: ViewEncapsulation.Emulated
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements 
+  OnInit, 
+  OnChanges, 
+  DoCheck, 
+  AfterContentInit, 
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy
+  {
   @Input('srvElement') element: { type: string, name: string, content: string };
-  constructor() { }
+  @Input() name: string;
+
+  @ViewChild('heading',{static: false}) header: ElementRef;
+  @ContentChild('contentParagraph', {static: true}) paragraph: ElementRef;
+ 
+  constructor() { 
+    console.log('constructor called');
+  }
+
+  ngOnChanges(changes: SimpleChanges) { 
+    console.log('ngOnChanges called');
+    console.log('changes', changes);
+  }
 
   ngOnInit(): void {
+    console.log('ngOnInit called');
+    //console.log("textContent", this.header.nativeElement.textContent);
+    // console.log("textContent paragraph", this.paragraph.nativeElement.textContent);
+  }
+
+  ngDoCheck() {
+    console.log("ngDoCheck called");
+  }
+
+  ngAfterContentInit() {
+    console.log("ngAfterContentInit called");
+    console.log("textContent paragraph", this.paragraph.nativeElement.textContent);
+  }
+
+  ngAfterContentChecked(): void {
+    console.log("ngAfterContentChecked called");
+  }
+
+  ngAfterViewChecked(): void {
+    console.log("ngAfterViewChecked called");
+  }
+  ngAfterViewInit(): void {
+    console.log("ngAfterViewInit called");
+    console.log("textContent", this.header.nativeElement.textContent);
+  }
+  
+  ngOnDestroy(): void {
+    console.log("ngOnDestroy");
   }
 
 }
