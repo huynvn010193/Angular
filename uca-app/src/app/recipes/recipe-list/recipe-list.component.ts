d876cfbe76022caa6c6a947fd2ac1c02e383ef61
huynvn <hpn010193@gmail.com>
2020-03-08 23:25:29 +0700

    Passing data with Event and Property Binding

--- uca-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ uca-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,14 +7,21 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe','A Test Recipe Desciption','https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg'),
-    new Recipe('A Test Recipe','A Test Recipe Desciption','https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg')
+    new Recipe('A Test Recipe 01','A Test Recipe Desciption 01','https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg'),
+    new Recipe('A Test Recipe 02','A Test Recipe Desciption 02','https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg')
   ];
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
+
 
 }
